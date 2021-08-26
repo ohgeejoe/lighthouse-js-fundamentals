b6e883ef626dfcf260923aed35cb3d4cc727a879
Joseph Shin <seoung.shin@ontariotechu.net>
2021-08-26 18:04:58 -0400

    x marks the spot function exercise. used for loop and array

---
+++ x-marks-the-perfect-shot.js
@@ -0,0 +1,33 @@
+//this program takes the parades movements by direction (N,S,E,W) given by variable "moves" and provides a grid co-ordinate spot.
+const moves = ['north', 'north', 'west', 'west', 'north', 'east','north'];
+
+const finalPosition = function (moves) {
+ for (let i = 0; i < moves.length; i++) {
+  let y = 0;
+  let x = 0;
+    if (moves[i] = "north") {
+      y++;
+    }
+    else if (moves[i] = "south") {
+      y--;
+    }
+    else if (moves[i] = "east") {
+      x++;
+    }
+    else if(moves[i] = "west") {
+      x--;
+    }    
+}
+return[x,y];
+}
+
+console.log(finalPosition(moves));
+
+/* for loop, N = y=++, S = y--, E = x ++, W = x-- 
+At the end return x,y
+
+need to iterate through al the array positions. and perform the increments.
+*/
+
+/*for (let i = 0; i < moves.length; i++)
+for (let i = 0; i < moves.length; i++)*/
