67922613a2f5a8474e297daf22037150f1bb1b69
Joseph Shin <seoung.shin@ontariotechu.net>
2021-08-26 14:13:32 -0400

    odd checker. Still getting the hang of git

---
+++ odd.js
@@ -0,0 +1,15 @@
+//This program evaluates if a number is odd or not. LHL Prepwork.
+
+function isOdd (result)
+{ 
+  if (result % 2 === 0)
+  {
+    return false;
+  }
+  else {
+    return true;
+  }
+}
+
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
